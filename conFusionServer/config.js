c85cdc018120f0fbff43589e0b916ca16c164762
Garvit Kumbhat <kumbhatgarvit919@gmail.com>
2022-05-12 13:12:53 +0530

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
-module.exports={
-    'secretKey':'12345-67890-09876-54321',
-    'mongoUrl':'mongodb://localhost:27017/conFusion'
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '732891567716614',
+        clientSecret: '13c0dc2c02612253643e417bddce4347'
+    }
 }
